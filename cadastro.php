e620a2c67e214869ebb0da8bab44171d5f25d184
Maria Livia Lima <marialiviadelimamoreira1@gmail.com>
2025-09-02 18:01:55 -0300

    Atualizando projeto com CSS e telas estilizadas

--- cadastro.php
+++ cadastro.php
@@ -1,41 +1,41 @@
 <?php
 include 'conexao.php';
-
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<form class="" action="cadastrar.php" method="post" enctype="multipart/form-data">
-   
-            <h1>Cadastrar Produtos</h1>
-           
-                <div class="textfield">
-                    <label for="Produto">Nick</label>
-                    <input type="text" name="produto" placeholder="Nome do Produto" required>
-                </div>
-                <div class="textfield">
-                    <label for="Data validade">Data de validade</label>
-                    <input type="text"  name="data_Validade" placeholder="Data de validade" required>
-                </div>
-                <div class="textfield">
-                    <label for="Preco">Preço</label>
-                    <input type="decimal"  name="preco" placeholder="Preço" required>
-                </div>
-                <button class="btn-cadastro">Cadastrar</button>
-                <br>
-                <a class="" href="index.php" >Voltar ao menu </a>
-                <br><br>
-                </form>
-            </div>
+  <div class="container d-flex flex-column justify-content-center align-items-center vh-100">
+    <div class="form-card">
+      <h1 class="mb-4">Cadastrar Produtos</h1>
+      <form action="cadastrar.php" method="post" enctype="multipart/form-data">
+        
+        <div class="textfield">
+          <label for="produto">Produto</label>
+          <input type="text" name="produto" id="produto" placeholder="Nome do Produto" required>
         </div>
+
+        <div class="textfield">
+          <label for="data_Validade">Data de validade</label>
+          <input type="text" name="data_Validade" id="data_Validade" placeholder="Data de validade" required>
+        </div>
+
+        <div class="textfield">
+          <label for="preco">Preço</label>
+          <input type="number" step="0.01" name="preco" id="preco" placeholder="Preço" required>
+        </div>
+
+        <button class="btn-cadastro" type="submit">Cadastrar</button>
+        <a class="btn-voltar" href="index.php">Voltar ao menu</a>
+      </form>
     </div>
-</form>
+  </div>
 </body>
 </html>
--- select.php
+++ select.php
@@ -5,31 +5,50 @@ $sql = "SELECT codigo, produto, data_validade, preco FROM produtos";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 
-
 $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<h2>Lista de Produtos</h2>
-<table border="1" cellpadding="8" cellspacing="0">
-    <tr>
-        <th>ID</th>
-        <th>Produto</th>
-        <th>Data de Validade</th>
-        <th>Preço</th>
-    </tr>
-    <?php if ($produtos): ?>
-        <?php foreach ($produtos as $p): ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Produtos Cadastrados</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="form-card">
+      <h1>Lista de Produtos</h1>
+      <table class="produtos-table">
+          <thead>
             <tr>
-                <td><?= $p['codigo'] ?></td>
-                <td><?= $p['produto'] ?></td>
-                <td><?= $p['data_validade'] ?></td>
-                <td><?= $p['preco'] ?></td>
+                <th>ID</th>
+                <th>Produto</th>
+                <th>Data de Validade</th>
+                <th>Preço</th>
             </tr>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <tr>
-            <td colspan="4">Nenhum produto cadastrado.</td>
-        </tr>
-    <?php endif; ?>
-</table>
-<a class="" href="index.php" >Voltar ao menu </a>
+          </thead>
+          <tbody>
+            <?php if ($produtos): ?>
+              <?php foreach ($produtos as $p): ?>
+                  <tr>
+                      <td><?= $p['codigo'] ?></td>
+                      <td><?= $p['produto'] ?></td>
+                      <td><?= $p['data_validade'] ?></td>
+                      <td>R$ <?= number_format($p['preco'], 2, ',', '.') ?></td>
+                  </tr>
+              <?php endforeach; ?>
+            <?php else: ?>
+              <tr>
+                  <td colspan="4">Nenhum produto cadastrado.</td>
+              </tr>
+            <?php endif; ?>
+          </tbody>
+      </table>
+      <a class="btn-voltar" href="index.php">Voltar ao menu</a>
+    </div>
+  </div>
+</body>
+</html>
